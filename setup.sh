25f7e9879ccabf6e891d623ce8c2359a181a0fdf
Rick Bordelon <rick.bordelon@redsailtechnologies.com>
2026-05-26 15:29:39 -0500

    Copy opencode.json from redsail-agents if an opencode.json file does not exist in current directory

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@
 cp -Rf redsail-agents/.agents ./.agents
 
 # Opencode
+if [ ! -f opencode.json ]; then
+  cp redsail-agents/opencode.json ./opencode.json
+  exit 0
+fi
+
 jq -s '.[0] * .[1]' redsail-agents/opencode.json opencode.json > opencode.json.tmp
 mv opencode.json.tmp opencode.json
 
