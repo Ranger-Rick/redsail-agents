f285852a2d882421778e3138a5c0579d7ea3d5fe
Rick Bordelon <rick.bordelon@redsailtechnologies.com>
2026-05-26 13:35:51 -0500

    Format the setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#Run from root directory. This script will copy the relevant contents of the redsail-agents up one directory
+# Run from root directory. This script will copy the relevant contents of the redsail-agents up one directory
 
 cp -Rf redsail-agents/.agents ./.agents
 
+# Opencode
 jq -s '.[0] * .[1]' redsail-agents/opencode.json opencode.json > opencode.json.tmp
 mv opencode.json.tmp opencode.json
 
+# TODO: Configure Claude, GitHub Copilot / VsCode
