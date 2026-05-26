b59bc18ed4e55ac21cc0ec5774a88586485d266b
Rick Bordelon <rick.bordelon@redsailtechnologies.com>
2026-05-26 13:20:07 -0500

    Updated the setup.sh script to apply new opencode configurations

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 
 #Run from root directory. This script will copy the relevant contents of the redsail-agents up one directory
 
-cp -Rf redsail-agents/.opencode ./.opencode
+cp -Rf redsail-agents/.agents ./.agents
+
+jq -s '.[0] * .[1]' redsail-agents/opencode.json opencode.json > opencode.json.tmp
+mv opencode.json.tmp opencode.json
+
